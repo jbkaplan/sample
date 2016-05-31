dc405643cd1f9d7f79c171bec835abe3973bdf3a
jbkaplan <jbkaplan@outlook.com>
2016-05-31 18:42:51 -0500

    Add styles and user controller

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20160528213242) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "hashed_password"
+    t.string   "password_digest"
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
